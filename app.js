d52fd5a4c1fdfacbfada7559b5c61d83f7e21c1a
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-02-08 20:33:47 +0100

    setup routing

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const postsRouter = require("routers/dolci.js")
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+    res.send("<h1> Rotta principale home")
+})
+
+
+
+app.use("/posts", postsRouter);
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
+
