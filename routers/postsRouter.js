3ef7f828e2892bf5802a755a26cf436f35608275
Andrea Barbuto <barbutoandrea99@gmail.com>
2026-02-10 09:47:43 +0100

    fix

---
+++ routers/postsRouter.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const router = express.Router();
+
+
+
+// index
+router.get("/", function (req, res) {
+    res.send("Lista dei post")
+})
+
+// show 
+router.get("/:id", function (req, res) {
+    res.send("Questo è il post numero " + req.params.id)
+})
+
+// store
+
+router.post("/", function (req, res) {
+    res.send("Crea un nuovo post")
+})
+
+// update
+
+router.put("/:id", function (req, res) {
+    res.send("Modifica integrale del post numero " + req.params.id)
+})
+
+// modify 
+
+router.patch("/:id", function (req, res) {
+    res.send("Modifica parziale del post numero " + req.params.id)
+})
+
+// delete
+
+router.delete("/:id", function (req, res) {
+    res.send("Eliminazione del post numero " + req.params.id)
+})
+module.exports = router;
